651c4bfa51cab91169ae9a207e8b9eb7167e3a60
unknown <doaram0610@gmail.com>
2022-04-20 14:52:18 +0900

    초기 데이터 생성 커미

---
+++ src/main/java/com/sds/hospapp/batch/DBInitializer.java
@@ -0,0 +1,80 @@
+package com.sds.hospapp.batch;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.sds.hospapp.domain.Hospital;
+import com.sds.hospapp.domain.HospitalRepository;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class DBInitializer {
+
+    //스프링부트 시작시에 한번 실행됨
+    @Bean
+    public CommandLineRunner initDB(HospitalRepository hospitalRepository){
+        return(args) ->{
+            //담을 그릇 준비
+            List<Hospital> hospitals = new ArrayList<>();
+                    
+            //api 한번 호출해서 totalcount 확인
+            RestTemplate rt = new RestTemplate();   //외부사이트 http접속
+            int totalCount = 2; // 1로 하면 리턴값 중 item 이 컬렉션이 아닌값으로 리턴되서 파싱에러난다. 그래서 2로 바꿈 
+
+            //호출할때 1건만 가져오기로 호출해도 전체카운드수를 리턴해준다.
+            URI totalCountUrl = new URI("http://apis.data.go.kr/B551182/rprtHospService/getRprtHospService?serviceKey=uckte7atD89qizM9shto778tPY%2BfxDlfusHvquvwdfLwNp06DF8sv9s6ml8zYHirqesrQrY2RTlxHUYVS%2FwUtQ%3D%3D&pageNo=1&numOfRows="
+            +totalCount+"&_type=json"); 
+
+            // String totalCountDto = rt.getForObject(totalCountUrl, String.class);
+            // System.out.println("가져온 데이터 totalCount : "+totalCountDto);
+
+            ResponseDto totalCountDto = rt.getForObject(totalCountUrl, ResponseDto.class);
+            totalCount = totalCountDto.getResponse().getBody().getTotalCount();
+            System.out.println("가져온 데이터 totalCount : "+totalCount);
+
+            // System.out.println("가져온 totalcount 값으로 전체데이터 추출 호출 시작 =============================");
+            // //totalCount 만큼 한번에 가져오기     
+            URI url = new URI("http://apis.data.go.kr/B551182/rprtHospService/getRprtHospService?serviceKey=uckte7atD89qizM9shto778tPY%2BfxDlfusHvquvwdfLwNp06DF8sv9s6ml8zYHirqesrQrY2RTlxHUYVS%2FwUtQ%3D%3D&pageNo=1&numOfRows="
+            +totalCount+"&_type=json");
+            ResponseDto dto = rt.getForObject(url, ResponseDto.class);
+
+            List<Item> items = dto.getResponse().getBody().getItems().getItem();
+            System.out.println("가져온 데이터 사이즈 : "+items.size());
+
+            //컬렉션 복사 (서버에서 받은 컬렉션 items 를 내가 DB에 저장할려구 만든 hospitals 에 복사)
+            //그런데 서버에서 받은 items 의 메소드와 내가 만든 hospitals의 메소드가 다르니까
+            //필요한 것만 hospitals에 넣으면서 복사한다.
+            hospitals = items.stream().map(
+                (e)->{
+                    return Hospital.builder()   //builder패턴은 생성자메소드보다 내가 선별해서 여러개를 생성할수 있어서 이걸 사용하는게 좋다
+                        .addr(e.getAddr())
+                        .mgtStaDd(e.getMgtStaDd())
+                        .pcrPsblYn(e.getPcrPsblYn())
+                        .ratPsblYn(e.getRatPsblYn())
+                        .recuClCd(e.getRecuClCd())
+                        .sgguCdNm(e.getSgguCdNm())
+                        .sidoCdNm(e.getSidoCdNm())
+                        .xPosWgs84(e.getXPosWgs84())
+                        .yPosWgs84(e.getYPosWgs84())
+                        .yadmNm(e.getYadmNm())
+                        .ykihoEnc(e.getYkihoEnc())
+                        .xPos(e.getXPos())
+                        .yPos(e.getYPos())                        
+                        .build();
+                }
+            ).collect(Collectors.toList()); //jdk8이상 스트림의 아이템들을 List 객체로 리턴
+
+            //기존 데이터 다 삭제하고 다시 입력하자
+            hospitalRepository.deleteAll();
+
+            //이제 담은 그릇의 데이터를 저장하자(1시간마다 실행)
+            hospitalRepository.saveAll(hospitals);            
+        };
+    }
+}
--- src/main/java/com/sds/hospapp/web/HospitalController.java
+++ src/main/java/com/sds/hospapp/web/HospitalController.java
@@ -7,11 +7,13 @@ import com.sds.hospapp.domain.HospitalRepository;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin
 @RequiredArgsConstructor    //final 객체의 생성자 만들기
 @Controller
 public class HospitalController {
